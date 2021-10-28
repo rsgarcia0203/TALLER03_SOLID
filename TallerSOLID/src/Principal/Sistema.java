b230f90856a6596ac3424758edf1683b5bb86020
leowipin <leonardopincayjaramillo@gmail.com>
2021-10-28 11:16:00 -0500

    manejo de la excepcion y adaptacion del main a nuestra implementacion

--- TallerSOLID/src/Principal/Sistema.java
+++ TallerSOLID/src/Principal/Sistema.java
@@ -4,46 +4,47 @@
  * and open the template in the editor.
  */
 package Principal;
+
 import Adicionales.Aderezo;
 import Adicionales.Crema;
 import Adicionales.Frutilla;
 import Postres.*;
 import Procesos.*;
 import Leche.*;
+import java.util.ArrayList;
+
 /**
  *
  * @author djurado
  */
 public class Sistema {
-    public static void main(String [ ] args){
-        // Producir un helado de vainilla y una torta de chocolate, 
-        // a ambos agregarles CREMA y FRUTILLAS
-        // y cambiar el tipo de leche por Leche Descremada
-        // Finalmente mostrar el precio final de cada uno
+
+    public static void main(String[] args) {
+        //producir un helado de vainilla y uno de torta de chocolate
+        //a ambos agregarle crema y frutilla
+        //y cambiar el tipo de leche por leche deslactosada
+        ArrayList<Postre> arrPostres = new ArrayList<>();
         LecheEntera leche = new LecheDeslactosada();
         ManejadorDeLeche mnj_leche = new ManejadorDeLeche();
-        
-        // Producir Helado
-       Postre helado_vainilla = new Helado("Vainilla");
-       Aderezo c= new Crema(); 
-       helado_vainilla.anadirAderezo( c);
-        Aderezo f = new Frutilla();
-        helado_vainilla.anadirAderezo( f);
-        System.out.println(helado_vainilla);
-//        mnj_leche.cambiarTipoLeche(leche, helado_vainilla);
-        System.out.println(ManejadorDePrecio.showPrecioFinal(helado_vainilla.getPrecioParcial(),helado_vainilla.getAderezos() ));
-       
-        // Producir Pastel
+
+        //producir helado
+        Postre helado_vainilla = new Helado("Vainilla");
+        arrPostres.add(helado_vainilla);
+        //producir pastel
         Postre pastel_chocolate = new Pastel("Chocolate");
-        pastel_chocolate.anadirAderezo(c);
-        pastel_chocolate.anadirAderezo(f);
-        System.out.println(pastel_chocolate);
-//        mnj_leche.cambiarTipoLeche(leche, pastel_chocolate);
-        System.out.println(ManejadorDePrecio.showPrecioFinal(pastel_chocolate.getPrecioParcial(),pastel_chocolate.getAderezos() ));
-        
-        
-        
-        
-        
+        arrPostres.add(pastel_chocolate);
+
+        arrPostres.forEach(postre -> {
+            postre.anadirAderezo(new Crema());
+            postre.anadirAderezo(new Frutilla());
+            System.out.println(postre);
+            try{
+            mnj_leche.cambiarTipoLeche(leche, postre);}
+            catch(RuntimeException re){
+                System.out.println(re.getMessage());
+            }
+            System.out.println(ManejadorDePrecio.showPrecioFinal(postre.getPrecioParcial(), postre.getAderezos()));
+        });
     }
+
 }
